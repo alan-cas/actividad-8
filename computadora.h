7a497fa3bfc14e71be5f1b0937a69663b7bbca19
Alan Castañeda Ascencio <alan.castaneda5829@alumnos.udg.mx>
2020-10-20 15:51:48 -0500

    flujos de entrada y salida

--- computadora.h
+++ computadora.h
@@ -1,6 +1,8 @@
 #ifndef COMPUTADORA_H
 #define COMPUTADORA_H
 #include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
 class Computadora{
     string nombre;
@@ -18,5 +20,42 @@ public:
     int getRam();
     void setPrecio(const float p);
     float getPrecio();
+    friend ostream& operator<<(ostream &out, const Computadora &compu)
+    {
+        out << setw(10) << compu.nombre
+            << setw(10) << compu.so
+            << setw(10) << compu.ram
+            << setw(10) << compu.precio;
+
+
+        return out;
+    }
+    friend istream& operator>>(istream &in, Computadora &compu)
+    {
+        string temp;
+        float temp_d;
+        int temp_i;
+
+
+        cout << "nombre: ";
+        getline(cin, temp);
+        compu.setNombre(temp);
+
+        cout << "sistema operativo: ";
+        getline(cin, temp);
+        compu.setSo(temp);
+
+        cout << "ram: ";
+        getline(cin, temp);
+        temp_i=stoi(temp);
+        compu.setRam(temp_i);
+
+        cout << "precio: ";
+        getline(cin, temp);
+        temp_d=stof(temp);
+        compu.setPrecio(temp_d);
+
+        return in;
+    }
 };
 #endif
