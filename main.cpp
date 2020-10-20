6210543c1dff86b305322a97bc826368e996f667
Alan Castañeda Ascencio <alan.castaneda5829@alumnos.udg.mx>
2020-10-20 15:54:59 -0500

    impresiones

--- main.cpp
+++ main.cpp
@@ -5,21 +5,14 @@ using namespace std;
 
 int main()
 {
-    Computadora c1;
+    Computadora c1,c3;
     Laboratorio l;
-    c1.setNombre("dell");
-    c1.setSo("linux");
-    c1.setRam(6);
-    c1.setPrecio(8000.00);
-    Computadora c2=Computadora("hp","window",2,3000.00);
-    Computadora c3=Computadora("mac","IOS",8,12000.00);
-    Computadora c4=Computadora("sony","linux",6,8000.00);
-    Computadora c5=Computadora("thinkpad","linux",6,7000.00);
-    l.agregarcomputadora(c1);
+    Computadora c2=Computadora("dell","windows",6,200.0);
     l.agregarcomputadora(c2);
-    l.agregarcomputadora(c3);
-    l.agregarcomputadora(c4);
-    l.agregarcomputadora(c5);
+    cin>>c3;
+    l<<c3;
     l.mostrar();
+    cin>>c1;
+    cout<<c1;
     return 0;
 }
