cc0f788eace3bc2eed3e8640f75046d01ae1aeac
Alan Castañeda Ascencio <alan.castaneda5829@alumnos.udg.mx>
2020-10-21 12:17:29 -0500

    impreciones de las nuevas funciones

--- main.cpp
+++ main.cpp
@@ -5,14 +5,24 @@ using namespace std;
 
 int main()
 {
-    Computadora c1,c3;
+    Computadora c1;
     Laboratorio l;
     Computadora c2=Computadora("dell","windows",6,200.0);
-    l.agregarcomputadora(c2);
-    cin>>c3;
+    Computadora c3=Computadora("hp","windows",4,100.0);
+    Computadora c4=Computadora("mac","windows",8,250.0);
+    l<<c2;
     l<<c3;
-    l.mostrar();
+    l<<c4;
+    l.respaldar();
     cin>>c1;
-    cout<<c1;
+    l<<c1;
+    cout<<"mostrar"<<endl;
+    l.mostrar();
+    cout<<"recuperar"<<endl;
+    l.recuperar();
+    cout<<"mostrar con los recuperados"<<endl;
+    l.mostrar();
+    cout<<"respaldar c1"<<endl;
+    l.respaldar();
     return 0;
 }
