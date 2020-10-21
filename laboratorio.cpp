78c692a8ce4f7f5a0c3cd1d205681d71b05127f4
Alan Castañeda Ascencio <alan.castaneda5829@alumnos.udg.mx>
2020-10-21 12:16:23 -0500

    implementacion, respaldar y recuperar

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -23,4 +23,61 @@ void Laboratorio::mostrar(){
         Computadora &compu = arreglo[i];
         cout << compu << endl;
     }
+
+}
+void Laboratorio::respaldar()
+{
+    ofstream archivo("computadora.txt");
+
+    if (archivo.is_open())
+    {
+        for (size_t i = 0; i < cont; i++)
+        {
+            Computadora &compu = arreglo[i];
+            archivo << compu.getNombre() << endl;
+            archivo << compu.getSo() << endl;
+            archivo << compu.getRam() << endl;
+            archivo << compu.getPrecio() << endl;
+        }
+
+    }
+    archivo.close();
+
+}
+void Laboratorio::recuperar()
+{
+    ifstream archivo("compuradoras.txt");
+
+    if (archivo.is_open())
+    {
+        Computadora compu;
+        string temp;
+        float temp_f;
+        int temp_i;
+
+        while (true)
+        {
+
+            getline(archivo, temp);
+            if (archivo.eof())
+            {
+            break;
+            }
+            compu.setNombre(temp);
+
+            getline(archivo, temp);
+            compu.setSo(temp);
+
+            getline(archivo, temp);
+            temp_i = stoi(temp);
+            compu.setRam(temp_i);
+
+            getline(archivo, temp);
+            temp_f = stof(temp);
+            compu.setPrecio(temp_f);
+
+            agregarcomputadora(compu);
+        }
+
+    }
 }
